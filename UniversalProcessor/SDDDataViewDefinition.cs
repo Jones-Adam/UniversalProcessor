d3d5a15762bc5e8a08040b610cf3740be177880e
Adam Jones <aquentin2033@outlook.com>
2019-09-19 23:49:41 +0200

    Simplied addition

--- UniversalProcessor/SDDDataViewDefinition.cs
+++ UniversalProcessor/SDDDataViewDefinition.cs
@@ -26,6 +26,22 @@ namespace UniversalProcessor
             Properties.Add(index, new SDDPropertyDefinition(propertyName, propertyType, CalculatorIndex));
         }
 
+        /// <summary>
+        /// Adds a new property to the object
+        /// </summary>
+        /// <param name="index">The identifier of the property</param>
+        /// <param name="propertyName">A name for the property</param>
+        /// <param name="propertyType">The property type</param>
+        /// <param name="func">A delegate to call to obtain the value of the property</param>
+        /// <returns>The id of the calculating function</returns>
+        public int Add(int index, string propertyName, Guid propertyType, Delegate func)
+        {
+            int calindex = GetFirstUnusedKey(Calculators);
+            Calculators.Add(calindex, func);
+            Properties.Add(index, new SDDPropertyDefinition(propertyName, propertyType, calindex));
+            return calindex;
+        }
+
         public SelfDescribingData GetProperty(int PropertyIdentifier)
         {
             SDDPropertyDefinition property = Properties[PropertyIdentifier];
@@ -46,5 +62,36 @@ namespace UniversalProcessor
         }
 
         public abstract string DisplayValue();
+
+        private static int GetFirstUnusedKey<TValue>(Dictionary<int, TValue> dict)
+        {
+            if (dict.Comparer != Comparer<int>.Default)
+                throw new NotSupportedException("Unsupported comparer");
+
+            using (var enumerator = dict.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                    return 0;
+
+                var nextKeyInSequence = enumerator.Current.Key + 1;
+
+                if (nextKeyInSequence < 1)
+                    throw new InvalidOperationException("The dictionary contains keys less than 0");
+
+                if (nextKeyInSequence != 1)
+                    return 0;
+
+                while (enumerator.MoveNext())
+                {
+                    var key = enumerator.Current.Key;
+                    if (key > nextKeyInSequence)
+                        return nextKeyInSequence;
+
+                    ++nextKeyInSequence;
+                }
+
+                return nextKeyInSequence;
+            }
+        }
     }
 }
